adb865f69d4b4c394db15b8c08290526e2561f27
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2020-09-10 16:40:44 +0600

    Toast Added

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +26,22 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview_layout);
         textView = findViewById(R.id.sampleId);
 
-        List<String> listItem = new ArrayList<String>();
+        final List<String> listItem = new ArrayList<String>();
         listItem.add("Andorid");
         listItem.add("Java");
         listItem.add("Kotlin");
 
         ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,R.layout.sample_view,R.id.sampleId,listItem);
         listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = listItem.get(i);
+
+                Toast.makeText(MainActivity.this,name,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
